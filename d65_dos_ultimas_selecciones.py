62cf14fc11a9d7a09cd730d18cf6f27e914c87a2
Adrián Gata <adgapor@gmail.com>
2025-03-28 11:37:51 +0100

    Update d65_dos_ultimas_selecciones.py

--- d65_dos_ultimas_selecciones.py
+++ d65_dos_ultimas_selecciones.py
@@ -1,20 +1,19 @@
 import pandas as pd
-import glob #para leer todo lo que haya en una carpeta
+import glob
 import os
 
-ruta = 'D:\\ADRIÁN\\Estudios\\Universidad UEX\\zzzTFG\\Datos\\*.xlsx' #importante las \\ para para evitar errores de escape en la cadena de la ruta
+ruta = 'D:\\ADRIÁN\\Estudios\\Universidad UEX\\zzzTFG\\Datos\\*.xlsx'
 
 dfs = []
 nombres_archivos = []
 
 for archivo in glob.glob(ruta):
     df = pd.read_excel(archivo)
-    dfs.append(df)  # Agrega el DataFrame a la lista
+    dfs.append(df)
 
     nombre_archivo_completo = os.path.basename(archivo)
-     # Eliminar la extensión del archivo y quitar el sufijo después del guion bajo
     nombre_archivo, _ = os.path.splitext(nombre_archivo_completo)
-    nombre_archivo = nombre_archivo.split('_')[0]  # Obtener solo la parte antes del guion bajo ()
+    nombre_archivo = nombre_archivo.split('_')[0]
     nombres_archivos.append(nombre_archivo)
 
 suma1=0
@@ -28,7 +27,7 @@ for i in range(len(dfs)):
     df = df[df['Lighting'] == 'D65']
     df=df.reset_index(drop=True)
     acierto = 'S05N (Instance)'
-    df['Resultado'] = df['Choose Option'].apply(lambda x: 1 if x == acierto else 0) #nueva columna: 1 es acierto, 0 es fallo
+    df['Resultado'] = df['Choose Option'].apply(lambda x: 1 if x == acierto else 0)
     
     if ((df['Time'] == 100) & (df['Resultado'] == 1)).any() and ((df['Time'] == 120) & (df['Resultado'] == 0)).any():
         print(nombres_archivos[i])
